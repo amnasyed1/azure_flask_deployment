70a4897dc928c7e8729c4bea2f74a71d621d0e9a
amnasyed1 <amna.syed@stonybrook.edu>
2023-09-17 19:22:27 +0000

    i added the dataset under the data file

--- app.py
+++ app.py
@@ -21,6 +21,12 @@ def randomnumber():
     fake_address = fake.address()
     return render_template('random.html', single_number=number_var, single_address =fake_address)
 
+df = pd.read_csv('https://raw.githubusercontent.com/amnasyed1/datasci_1_loading/main/data/newborn-screening-disorders-californiaraceethnicity-2009-2019_final.csv')
+@app.route('/data')
+def data(data=df):
+    data = data.sample(15)
+    return render_template('data.html', data=data)
+
 if __name__ == '__main__':
     app.run(
         debug=True,
