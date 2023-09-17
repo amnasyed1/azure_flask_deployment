85f93b2538b9b92b70f277d78e9f8d73fd13e066
amnasyed1 <amna.syed@stonybrook.edu>
2023-09-17 19:14:04 +0000

    added random number and address

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template
 import pandas as pd
+import random 
+from faker import Faker
+
+fake = Faker()
 
 app = Flask(__name__)
 
@@ -10,6 +14,12 @@ def mainpage():
 @app.route('/about')
 def about():
     return render_template('about.html') 
+
+@app.route('/random')
+def randomnumber():
+    number_var = random.randint(1, 10000)
+    fake_address = fake.address()
+    return render_template('random.html', single_number=number_var, single_address =fake_address)
 
 if __name__ == '__main__':
     app.run(
